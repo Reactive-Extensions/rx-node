a1805619895211d17a56aa034baafef86ec0cc57
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-04-01 01:26:05 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,141 @@
+var Rx = require('rx');
+
+// Add specific Node functions
+var EventEmitter = require('events').EventEmitter, Observable = Rx.Observable;
+
+module.exports = {
+
+  /**
+   * Converts the given observable sequence to an event emitter with the given event name.
+   * The errors are handled on the 'error' event and completion on the 'end' event.
+   * @param {Observable} observable The observable sequence to convert to an EventEmitter.
+   * @param {String} eventName The event name to emit onNext calls.
+   * @returns {EventEmitter} An EventEmitter which emits the given eventName for each onNext call in addition to 'error' and 'end' events.
+   *   You must call publish in order to invoke the subscription on the Observable sequuence.
+   */
+  toEventEmitter: function (observable, eventName, selector) {
+    var e = new EventEmitter();
+
+    // Used to publish the events from the observable
+    e.publish = function () {
+      e.subscription = observable.subscribe(
+        function (x) {
+          var result = x;
+          if (selector) {
+            try {
+              result = selector(x);
+            } catch (e) {
+              return e.emit('error', e);
+            }
+          }
+
+          e.emit(eventName, result);
+        },
+        function (err) {
+          e.emit('error', err);
+        },
+        function () {
+          e.emit('end');
+        });
+    };
+
+    return e;
+  },
+
+  /**
+   * Converts a flowing stream to an Observable sequence.
+   * @param {Stream} stream A stream to convert to a observable sequence.
+   * @param {String} [finishEventName] Event that notifies about closed stream. ("end" by default)
+   * @returns {Observable} An observable sequence which fires on each 'data' event as well as handling 'error' and finish events like `end` or `finish`.
+   */
+  fromStream: function (stream, finishEventName) {
+    stream.pause();
+
+    finishEventName || (finishEventName = 'end');
+
+    return Observable.create(function (observer) {
+      function dataHandler (data) {
+        observer.onNext(data);
+      }
+
+      function errorHandler (err) {
+        observer.onError(err);
+      }
+
+      function endHandler () {
+        observer.onCompleted();
+      }
+
+      stream.addListener('data', dataHandler);
+      stream.addListener('error', errorHandler);
+      stream.addListener(finishEventName, endHandler);
+
+      stream.resume();
+
+      return function () {
+        stream.removeListener('data', dataHandler);
+        stream.removeListener('error', errorHandler);
+        stream.removeListener(finishEventName, endHandler);
+      };
+    }).publish().refCount();
+  },
+
+  /**
+   * Converts a flowing readable stream to an Observable sequence.
+   * @param {Stream} stream A stream to convert to a observable sequence.
+   * @returns {Observable} An observable sequence which fires on each 'data' event as well as handling 'error' and 'end' events.
+   */
+  fromReadableStream: function (stream) {
+    return this.fromStream(stream, 'end');
+  },
+
+  /**
+   * Converts a flowing writeable stream to an Observable sequence.
+   * @param {Stream} stream A stream to convert to a observable sequence.
+   * @returns {Observable} An observable sequence which fires on each 'data' event as well as handling 'error' and 'finish' events.
+   */
+  fromWritableStream: function (stream) {
+    return this.fromStream(stream, 'finish');
+  },
+
+  /**
+   * Converts a flowing transform stream to an Observable sequence.
+   * @param {Stream} stream A stream to convert to a observable sequence.
+   * @returns {Observable} An observable sequence which fires on each 'data' event as well as handling 'error' and 'finish' events.
+   */
+  fromTransformStream: function (stream) {
+    return this.fromStream(stream, 'finish');
+  },
+
+  /**
+   * Writes an observable sequence to a stream
+   * @param {Observable} observable Observable sequence to write to a stream.
+   * @param {Stream} stream The stream to write to.
+   * @param {String} [encoding] The encoding of the item to write.
+   * @returns {Disposable} The subscription handle.
+   */
+  writeToStream: function (observable, stream, encoding) {
+    var source = observable.pausableBuffered();
+
+    function onDrain() {
+      source.resume();
+    }
+
+    stream.addListener('drain', onDrain);
+
+    return source.subscribe(
+      function (x) {
+        !stream.write(String(x), encoding) && source.pause();
+      },
+      function (err) {
+        stream.emit('error', err);
+      },
+      function () {
+        // Hack check because STDIO is not closable
+        !stream._isStdio && stream.end();
+        stream.removeListener('drain', onDrain);
+      });
+
+    source.resume();
+  }
+};
---
+++ readme.md
@@ -0,0 +1,45 @@
+# rx-node - RxJS bindings for Node.js and io.js #
+
+## OVERVIEW
+
+This project provides Reactive Extensions for JavaScript (RxJS) bindings for [Node.js](http://nodejs.org) and [io.js](https://iojs.org) to abstract over the `EventEmitter`, Streams and more.  
+
+## GETTING STARTED
+
+There are a number of ways to get started with the RxJS bindings for Node.js and io.js.
+
+### Download the Source
+
+To download the source of the HTML DOM Bindings for the Reactive Extensions for JavaScript, type in the following:
+```bash
+git clone https://github.com/Reactive-Extensions/rx-node.git
+cd ./rxjs-dom
+```
+### Installing with [NPM](https://npmjs.org/)
+```bash
+npm install rx-dom
+```
+##  API Documentation ##
+
+You can find the documentation [here](https://github.com/Reactive-Extensions/rx-node/tree/master/doc) as well as examples [here](https://github.com/Reactive-Extensions/rx-node/tree/master/examples).
+
+## Contributing ##
+
+There are lots of ways to [contribute](https://github.com/Reactive-Extensions/rx-node/wiki/Contributing) to the project, and we appreciate our [contributors](https://github.com/Reactive-Extensions/rx-node/wiki/Contributors).
+
+You can contribute by reviewing and sending feedback on code checkins, suggesting and trying out new features as they are implemented, submit bugs and help us verify fixes as they are checked in, as well as submit code fixes or code contributions of your own. Note that all code submissions will be rigorously reviewed and tested by the Rx Team, and only those that meet an extremely high bar for both quality and design/roadmap appropriateness will be merged into the source.
+
+## LICENSE
+
+Copyright Microsoft Open Technologies
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
